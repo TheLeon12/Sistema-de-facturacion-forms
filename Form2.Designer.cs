5430e41fb6cad175cf914adff683589c4d34e141
alexa <alexa@DESKTOP-ITEJDQK>
2025-06-02 14:56:32 -0400

    fff

---
+++ Form2.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace Sistema_de_facturacion
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            label7 = new Label();
+            textBox7 = new TextBox();
+            label8 = new Label();
+            textBox8 = new TextBox();
+            label9 = new Label();
+            textBox9 = new TextBox();
+            label10 = new Label();
+            textBox10 = new TextBox();
+            label11 = new Label();
+            textBox11 = new TextBox();
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(363, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre:";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(451, 23);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(319, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(337, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Descripción:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(451, 86);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(122, 31);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(584, 89);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Stock:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(648, 86);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(122, 31);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(353, 154);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Categoría:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(451, 154);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(319, 31);
+            textBox4.TabIndex = 7;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(358, 223);
+            label5.Name = "label5";
+            label5.Size = new Size(87, 25);
+            label5.TabIndex = 8;
+            label5.Text = "Cantidad:";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(451, 217);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(122, 31);
+            textBox5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(584, 220);
+            label6.Name = "label6";
+            label6.Size = new Size(64, 25);
+            label6.TabIndex = 10;
+            label6.Text = "Precio:";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(648, 217);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(122, 31);
+            textBox6.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(314, 275);
+            label7.Name = "label7";
+            label7.Size = new Size(131, 25);
+            label7.TabIndex = 12;
+            label7.Text = "Precio Unitario:";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(451, 272);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(122, 31);
+            textBox7.TabIndex = 13;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(588, 275);
+            label8.Name = "label8";
+            label8.Size = new Size(55, 25);
+            label8.TabIndex = 14;
+            label8.Text = "ITBIS:";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(648, 272);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(122, 31);
+            textBox8.TabIndex = 15;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(312, 331);
+            label9.Name = "label9";
+            label9.Size = new Size(133, 25);
+            label9.TabIndex = 16;
+            label9.Text = "Precio Por Caja:";
+            label9.Click += label9_Click;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(451, 328);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(122, 31);
+            textBox9.TabIndex = 17;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(588, 328);
+            label10.Name = "label10";
+            label10.Size = new Size(64, 25);
+            label10.TabIndex = 18;
+            label10.Text = "Marca:";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(648, 322);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(122, 31);
+            textBox10.TabIndex = 19;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(263, 382);
+            label11.Name = "label11";
+            label11.Size = new Size(182, 25);
+            label11.TabIndex = 20;
+            label11.Text = "Precio Unitario x Caja:";
+            label11.Click += label11_Click;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(451, 382);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(122, 31);
+            textBox11.TabIndex = 21;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(612, 384);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(88, 29);
+            checkBox1.TabIndex = 22;
+            checkBox1.Text = "Activo";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(531, 435);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 23;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(0, 0);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 24;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(402, 510);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 25;
+            button3.Text = "Agregar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(531, 510);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 34);
+            button4.TabIndex = 26;
+            button4.Text = "Editar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(658, 510);
+            button5.Name = "button5";
+            button5.Size = new Size(112, 34);
+            button5.TabIndex = 27;
+            button5.Text = "Eliminar";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 607);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox11);
+            Controls.Add(label11);
+            Controls.Add(textBox10);
+            Controls.Add(label10);
+            Controls.Add(textBox9);
+            Controls.Add(label9);
+            Controls.Add(textBox8);
+            Controls.Add(label8);
+            Controls.Add(textBox7);
+            Controls.Add(label7);
+            Controls.Add(textBox6);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = " ";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox6;
+        private Label label7;
+        private TextBox textBox7;
+        private Label label8;
+        private TextBox textBox8;
+        private Label label9;
+        private TextBox textBox9;
+        private Label label10;
+        private TextBox textBox10;
+        private Label label11;
+        private TextBox textBox11;
+        private CheckBox checkBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
