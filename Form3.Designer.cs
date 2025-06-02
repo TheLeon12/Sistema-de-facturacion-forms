14b59e891f84fb123e5ecd631e2559db0b28ef5e
alexa <alexa@DESKTOP-ITEJDQK>
2025-06-02 15:59:35 -0400

    Forms Ventas

---
+++ Form3.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Sistema_de_facturacion
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(298, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Clientes:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(393, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(371, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(314, 81);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Fecha:";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(393, 81);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(371, 31);
+            dateTimePicker1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(286, 136);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Producto:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(393, 133);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(136, 31);
+            textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(535, 136);
+            label4.Name = "label4";
+            label4.Size = new Size(87, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Cantidad:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(628, 133);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(136, 31);
+            textBox3.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4 });
+            dataGridView1.Location = new Point(220, 204);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(609, 225);
+            dataGridView1.TabIndex = 8;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Productos ";
+            Column1.MinimumWidth = 8;
+            Column1.Name = "Column1";
+            Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Cantidad ";
+            Column2.MinimumWidth = 8;
+            Column2.Name = "Column2";
+            Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Precio Unitario";
+            Column3.MinimumWidth = 8;
+            Column3.Name = "Column3";
+            Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Subtotal";
+            Column4.MinimumWidth = 8;
+            Column4.Name = "Column4";
+            Column4.Width = 150;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(279, 455);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(136, 31);
+            textBox4.TabIndex = 10;
+            textBox4.TextChanged += this.textBox4_TextChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(220, 455);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 25);
+            label5.TabIndex = 9;
+            label5.Text = "Total:";
+            label5.Click += this.label5_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(347, 522);
+            button1.Name = "button1";
+            button1.Size = new Size(157, 34);
+            button1.TabIndex = 11;
+            button1.Text = "Guardar Venta ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(535, 522);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 12;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(852, 594);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form3";
+            Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private DateTimePicker dateTimePicker1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private TextBox textBox4;
+        private Label label5;
+        private Button button1;
+        private Button button2;
+    }
+}
